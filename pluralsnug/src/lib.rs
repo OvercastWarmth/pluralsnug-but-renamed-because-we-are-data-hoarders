f3e965f342a8e268a59f5ea0f837edc3a6e0e7f0
Gem <110815694+Ringlings@users.noreply.github.com>
2023-09-16 20:29:57 +1000

    Saving, path configuration, friendly errors
    SystemPathRanOutOfOptionsError my beloved
    
    Co-authored-by: Fnige <Fnige@users.noreply.github.com>

--- pluralsnug/src/lib.rs
+++ pluralsnug/src/lib.rs
@@ -1,22 +1,40 @@
-use serde::Deserialize;
-use std::{error::Error, fs, path::Path};
+use serde::{Deserialize, Serialize};
+use std::{
+	error::Error,
+	fs,
+	path::{Path, PathBuf},
+};
 
 /// A PluralSnug system.
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Serialize)]
 pub struct System {
 	/// The system's name
 	pub name: Option<String>,
 }
 
 /// Loads a PluralSnug system.
-pub fn load<P: AsRef<Path>>(path: P) -> Result<System, Box<dyn Error>> {
-	Ok(json5::from_str(fs::read_to_string(path)?.as_str())?)
+pub fn load(path: &PathBuf) -> Result<System, Box<dyn Error>> {
+	let content = match fs::read_to_string(&path) {
+		Ok(content) => content,
+		Err(error) => Err(format!(
+			"Could not load PluralSnug system at {:?}\nOriginal Error: {}",
+			path, error
+		))?,
+	};
+
+	let system = match json5::from_str(content.as_str()) {
+		Ok(system) => system,
+		Err(error) => Err(format!(
+			"Could not load PluralSnug system: JSON5 is invalid\nOriginal Error: {}",
+			error
+		))?,
+	};
+
+	Ok(system)
 }
 
 /// Saves a PluralSnug system.
-///
-/// Currently not functioning
-#[allow(unused_variables)]
 pub fn save<P: AsRef<Path>>(system: System, path: P) -> Result<(), Box<dyn Error>> {
+	fs::write(path, json5::to_string(&system)?)?;
 	Ok(())
 }
--- pluralsnug_cli/src/lib.rs
+++ pluralsnug_cli/src/lib.rs
@@ -1,11 +1,13 @@
-use std::error::Error;
-
 use clap::{Parser, Subcommand};
-
+use std::{env, error::Error, fmt::Display, path::PathBuf};
 #[derive(Parser, Debug)]
 pub struct Cli {
 	#[command(subcommand)]
 	target: Target,
+
+	// TODO: Figure out how to bring flags to the back
+	#[arg(short, long)]
+	config: Option<PathBuf>,
 }
 
 #[derive(Subcommand, Debug)]
@@ -31,9 +33,11 @@ enum Action {
 }
 
 pub fn run(cli: Cli) -> Result<(), Box<dyn Error>> {
+	let path = configure(&cli)?;
+
 	match cli.target {
 		Target::System { field } => {
-			let mut system = pluralsnug::load("tests/test.json")?;
+			let mut system = pluralsnug::load(&path)?;
 			println!("{:?}", system); // TEMP
 
 			match field {
@@ -58,9 +62,39 @@ pub fn run(cli: Cli) -> Result<(), Box<dyn Error>> {
 				}
 			}
 
-			pluralsnug::save(system, "tests/test.conf")?;
+			pluralsnug::save(system, path)?;
 		}
 	}
 
 	Ok(())
 }
+
+fn configure(cli: &Cli) -> Result<PathBuf, Box<dyn Error>> {
+	let path: PathBuf = match &cli.config {
+		Some(path) => path.clone(),
+		None => match env::var("PLURALSNUG_PATH") {
+			Ok(path) => PathBuf::from(path),
+			Err(_) => match home::home_dir() {
+				// Some(dir) => PathBuf::from(dir).join(".pluralsnug/system.json5"),
+				Some(_) => return Err(Box::new(SystemPathRanOutOfOptionsError)),
+				None => return Err(Box::new(SystemPathRanOutOfOptionsError)),
+			},
+		},
+	};
+
+	Ok(path)
+}
+
+#[derive(Debug)]
+struct SystemPathRanOutOfOptionsError;
+
+impl Display for SystemPathRanOutOfOptionsError {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(
+			f,
+			"You don't have a home path and we've run out of options.\nSet the PLURALSNUG_PATH environment variable to set where you want your PluralSnug database to be"
+		)
+	}
+}
+
+impl Error for SystemPathRanOutOfOptionsError {}
